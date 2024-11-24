e983591821534823feea2c1be742d763f461abd0
Tanvir Anjum Neon <taneon88@gmail.com>
2024-11-24 16:23:43 +0600

    remove old job

--- [qb]/[JOBS]/m-postop/insert-me.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `m_postop` (
-	`identifier` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`userdata` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci'
-)
-COLLATE='utf8mb3_general_ci'
-ENGINE=InnoDB
-;
